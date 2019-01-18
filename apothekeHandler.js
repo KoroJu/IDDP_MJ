78a68435c64b224352c20383b854162392218926
holmma <Marvin.Holm@Student.Reutlingen-University.de>
2019-01-18 18:06:23 +0100

    es werden nicht mehr hinzugefügt bei mehrmaligen klicken

--- apothekeHandler.js
+++ apothekeHandler.js
@@ -1,8 +1,15 @@
 function searchPrescription(){
   var number = document.getElementById("insurancePolicyNumber").value;
+  var table = document.getElementById("prescription");
 /*  if(number == ""){
     return;
   }*/
+  console.log(table.rows.length-1);
+  var j = table.rows.length-1;
+  while(j != 0){
+    table.deleteRow(j);
+    j--;
+  }
 
   var url
   var getRequest = new XMLHttpRequest();
@@ -11,15 +18,15 @@ function searchPrescription(){
   getRequest.addEventListener("load", () => {
     const parsedData = JSON.parse(getRequest.responseText);
     console.log(parsedData);
-    for(var i = 0; i<parsedData.length;i++){    
-      var table = document.getElementById("prescription");
+    for(var i = 0; i<parsedData.length;i++){
+      console.log(table.rows.length-1);
       var row = table.insertRow(1);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
       cell1.innerHTML = parsedData[i].insurancePolicyNumber;
-      cell2.innerHTML = parsedData[i].description;
-      cell3.innerHTML = parsedData[i].date;}
+      cell2.innerHTML = parsedData[i].date;
+      cell3.innerHTML = parsedData[i].description;}
   });
 
   getRequest.open("GET", "http://127.0.0.1:4000/api/prescription/"+number);
