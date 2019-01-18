84dc371ffb9ec7a0761e83800e5842b2708f8ab7
holmma <Marvin.Holm@Student.Reutlingen-University.de>
2019-01-18 17:47:51 +0100

    ansicht apotheke

--- apothekeHandler.js
+++ apothekeHandler.js
@@ -11,14 +11,15 @@ function searchPrescription(){
   getRequest.addEventListener("load", () => {
     const parsedData = JSON.parse(getRequest.responseText);
     console.log(parsedData);
-    var table = document.getElementById("prescription");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(1);
-    cell1.innerHTML = parsedData[0].insurancePolicyNumber;
-    cell2.innerHTML = parsedData[0].description;
-    cell3.innerHTML = parsedData[0].date;
+    for(var i = 0; i<parsedData.length;i++){    
+      var table = document.getElementById("prescription");
+      var row = table.insertRow(1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(1);
+      cell1.innerHTML = parsedData[i].insurancePolicyNumber;
+      cell2.innerHTML = parsedData[i].description;
+      cell3.innerHTML = parsedData[i].date;}
   });
 
   getRequest.open("GET", "http://127.0.0.1:4000/api/prescription/"+number);
