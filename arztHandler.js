ab3bdf955ba3da0d838335c3b56cfeaf0a82f18a
Julian Koronai <jm.koronai@mail.de>
2019-01-18 17:19:51 +0100

    addPrescription()  und getPatients()

--- arztHandler.js
+++ arztHandler.js
@@ -1,4 +1,8 @@
 var patients = [];
+var currentDoc = "5c41f6f0281e7154e8456e6b";
+
+
+
 
 // function addPatient(){
 //
@@ -15,11 +19,11 @@ var patients = [];
 function addPrescription()  {
 
     //hardcoded doctor
-    // var doc = {"firstname":"Bob", "lastname":"Kelso", "personalnumber":"BK-666"}
+    //var doc = {"firstname":"Bob", "lastname":"Kelso", "personalnumber":"BK-666"}
     var doc = "BK-666"
     var desc = document.getElementById("prescriptionDesc").value;
       var pats = document.getElementById("patientDropDown");
-    var selPatID = pats.options[pats.selectedIndex].text;
+    // var selPatID = pats.options[pats.selectedIndex].text;
 
     var today = new Date();
     var dd = today.getDate();
@@ -36,9 +40,9 @@ function addPrescription()  {
 
     var url = "http://127.0.0.1:4000/api/prescription";
     var data = {};
-    data.Patient = selPatID;
-    data.Doctor  = doc;
-    data.description = desc:
+    data.insurancePolicyNumber = "testidhaltjetzt" ;    //selPatID
+    data.personalnumber  = doc;
+    data.description = desc;
     data.date = today;
     var json = JSON.stringify(data);
 
@@ -47,10 +51,12 @@ function addPrescription()  {
     xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
     xhr.onload = function () {
 	     var prescriptions = JSON.parse(xhr.responseText);
-	      if (xhr.readyState == 4 && xhr.status == "201") {
+	      if (xhr.readyState == 4 && xhr.status == "200") {
 		        console.table(prescriptions);
 	      } else {
 		        console.error(prescriptions);
+            console.log(xhr.readyState);
+            console.log(xhr.status);
 	         }
     }
     xhr.send(json);
@@ -61,39 +67,31 @@ function addPrescription()  {
 
 function getPatients(){
 
-  var url  = "http://127.0.0.1:4000/api/users";
+  var url  = "http://127.0.0.1:4000/api/users/" + currentDoc;
   var xhr  = new XMLHttpRequest()
   xhr.open('GET', url, true)
   xhr.onload = function () {
-	   var patients = JSON.parse(xhr.responseText);
-	    if (xhr.readyState == 4 && xhr.status == "200") {
-		      console.table(users);
-	       } else {
-		         console.error(users);
-	          }
+     var patients = JSON.parse(xhr.responseText);
+      if (xhr.readyState == 4 && xhr.status == "200") {
+          console.table(patients);
+          console.log(patients.doctor.insurancePolicyNumber.length);
+
+          var select = document.getElementById("patientDropDown");
+          var ids = patients.doctor.insurancePolicyNumber;
+          // for each patient element put ID in ids
+
+
+          for(var i = 0; i < ids.length; i++) {       //erstellt für jeden Patienten ein Element
+            var id = ids[i];                          // im Dropdown Menü mit Patient ID
+            var elem = document.createElement("option");
+            elem.textContent = id;
+            elem.value = id;
+            select.appendChild(elem);
+          }
+         } else {
+             console.error(patients);
+            }
   }
   xhr.send(null);
-
-
-
-
-
-
-  var select = document.getElementById("patientDropDown");
-  var ids = []
-  // for each patient element put ID in ids
-
-
-  for(var i = 0; i < ids.length; i++) {       //erstellt für jeden Patienten ein Element
-    var id = ids[i];                          // im Dropdown Menü mit Patient ID
-    var elem = document.createElement("option");
-    elem.textContent = id;
-    elem.value = id;
-    select.appendChild(id);
-  }​
-
-
-
-
 
 }
